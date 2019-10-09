6cb17a8c3a405aab49b1f870b9d436991bf4a827
Adrian <a>
2019-10-09 12:34:55 +0200

    Send User || Disfuncional

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
+import { User } from 'src/model/user';
+import { UsuarioRestService } from '../usuario-rest.service';
 
 @Component({
   selector: 'app-usuario',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent implements OnInit {
-  modoEdicion = true;
-  constructor() { }
+  modoEdicion: boolean;
+  userSelected: User;
+  modoBorrar: boolean;
+  users: User[];
+
+  name: string;
+  age: string;
+  email: string;
+  password: string;
+  constructor(private userService: UsuarioService, private userRestService: UsuarioRestService) { }
 
   ngOnInit() {
+    this.getUsuarios();
+    this.modoEdicion = false;
+    this.modoBorrar = false;
   }
+  getUsuarios() {
+    this.userService.getUsuarios().subscribe(user => this.users = user);
+  }
+  onSelect(user: User) {
+    this.userSelected = user;
 
+  }
+  enableEdition() {
+    this.modoEdicion = !this.modoEdicion;
+  }
+  enableDeleteById() {
+    this.modoBorrar = !this.modoBorrar;
+  }
+  
+  sendUser() {
+    let nuevoUser = new User();
+    nuevoUser.name = this.name;
+    nuevoUser.email = this.email;
+    nuevoUser.password = this.password;
+    nuevoUser.age = parseInt(this.age);
+    console.log(nuevoUser.name + ' email: ' + nuevoUser.email);
+    
+    this.userRestService.add(nuevoUser);
+  }
 }
--- src/model/array-pruebas.ts
+++ src/model/array-pruebas.ts
@@ -1,8 +1,8 @@
 import { User } from './user';
 
 export const USUARIOS: User[] = [
-    { id: 1, name: 'Antonio', email: 'antonio@gmail.com', age: 23, '1234' },
-    { id: 2, name: 'Marcos', email: 'marcos@gmail.com', age: 32, '1234' },
-    { id: 3, name: 'David', email: 'david@gmail.com', age: 26, '1234' },
-    { id: 4, name: 'Adrián', email: 'adrian@gmail.com', age: 31, '1234' }
+    { id: 1, name: 'Antonio', email: 'antonio@gmail.com', age: 23, password: '1234'},
+    { id: 2, name: 'Marcos', email: 'marcos@gmail.com', age: 32, password: '1234' },
+    { id: 3, name: 'David', email: 'david@gmail.com', age: 26, password: '1234' },
+    { id: 4, name: 'Adrián', email: 'adrian@gmail.com', age: 31, password: '1234' }
 ];
